11c4025fe8f734acb0381ee202b46b2e386dd814
Fsmv <kallmeyeras@gmail.com>
2012-08-24 22:29:12 -0400

    Made key holding work for WASD

--- src/com/voracious/ld24/screens/Play.java
+++ src/com/voracious/ld24/screens/Play.java
@@ -11,6 +11,7 @@ public class Play extends Screen {
 	
 	private static ArrayList<Integer> heightMap = new ArrayList<Integer>();
 	private static int offsetX = 0;
+	private static boolean[] keysDown = {false, false, false, false}; //w, a, s, d
 
 	public Play(int width, int height) {
 		super(width, height);
@@ -26,7 +27,7 @@ public class Play extends Screen {
     }
     
     public void generateLevel(float difficulty){
-    	int size = (int) (difficulty * 512);
+    	int size = (int) (difficulty * 0xfff);
     	
     	for(int i=0; i<size; i++){
     		heightMap.add(Integer.valueOf(Math.abs((int) (Math.sin(i/100.0)*75))));
@@ -45,18 +46,38 @@ public class Play extends Screen {
     }
     
     public void tick() {
-    	
+    	if(keysDown[1]){ //a
+    		if(offsetX > 5){
+    			offsetX -= 5;
+    		}
+    	}else if(keysDown[3]){ //d
+    		if(offsetX < heightMap.size() - this.getWidth() - 5){
+    			offsetX += 5;
+    		}
+    	}
+    }
+    
+    public void keyPressed(KeyEvent e){
+    	if(e.getKeyChar() == 'w'){
+    		keysDown[0] = true;
+    	}else if(e.getKeyChar() == 'a') {
+            keysDown[1] = true;
+        }else if(e.getKeyChar() == 's'){
+        	keysDown[2] = true;
+        }else if(e.getKeyChar() == 'd') {
+            keysDown[3] = true;
+        }
     }
     
     public void keyReleased(KeyEvent e){
-        if(e.getKeyChar() == 'a') {
-            if(offsetX > 5){
-            	offsetX-=5;
-            }
-        }else if(e.getKeyChar() == 'd'){
-        	if(offsetX < heightMap.size() - 205){
-        		offsetX+=5;
-        	}
+    	if(e.getKeyChar() == 'w'){
+    		keysDown[0] = false;
+    	}else if(e.getKeyChar() == 'a') {
+            keysDown[1] = false;
+        }else if(e.getKeyChar() == 's'){
+        	keysDown[2] = false;
+        }else if(e.getKeyChar() == 'd') {
+            keysDown[3] = false;
         }
     }
 }
